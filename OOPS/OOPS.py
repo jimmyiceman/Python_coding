86102e968186e1665944da8748b070e21c05e9df
jimmyiceman <jimmy.iceman@gmail.com>
2016-04-14 14:28:23 +0530

    Calculator class (simple)

---
+++ OOPS/OOPS.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 14 14:22:06 2016
+Objective: OOPS concepts 
+@author: jatan.sharma
+"""
+
+class calculator:
+    
+    def addition(x,y):
+        added = x + y
+        print(added)
+        
+    def subtraction(x,y):
+        sub = x - y
+        print(sub)
+
+    def multiplication(x,y):
+        mult = x * y
+        print(mult)
+
+    def division(x,y):
+        div = x / y
+        print(div)
+        
+        
+calculator.addition(5,8)
+calculator.division(4,2)
