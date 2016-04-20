7b932272e34ffc9d5aa8701aa8ee176e95eb127e
jimmyiceman <jimmy.iceman@gmail.com>
2016-04-20 13:12:26 +0530

    List Manipulation research

--- Data manipulation/List_manipulation.py
+++ Data manipulation/List_manipulation.py
@@ -5,3 +5,24 @@ Objective: List manipulation
 @author: jatan.sharma
 """
 
+# first we need an example list:
+x = [1,6,3,2,6,1,2,6,7]
+# lets add something.
+# we can do .append, which will add something to the end of the list, like:
+x.append(55)
+print(x)
+
+# Insert in between
+x.insert(2,33)
+print(x)
+
+# Remove element from the list
+x.remove(6)
+print(x)
+
+print(x[5])
+print(x.index(1))
+print(x.count(1))
+  
+x.sort()
+print(x)
---
+++ Data manipulation/multi_dimensional_lists.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 12:59:10 2016
+Objective: Multi-dimensional lists 
+@author: jatan.sharma
+"""
+
